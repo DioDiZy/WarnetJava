eeac203c944bbca79c7533493a26a44873a3ee32
DioDiZy <104825699+DioDiZy@users.noreply.github.com>
2024-04-06 00:53:00 +0700

    Penambahan Komentar Visibility

--- Tubes_Kelompok/Umum.java
+++ Tubes_Kelompok/Umum.java
@@ -1,4 +1,5 @@
 // Implementasi kelas turunan dari kelas member
+// Kelas Member, VIP, Umum mengimplementasikan Visibility yang dimana kelasnya hanya dapat diacces oleh orangtua serta turunannya saja
 public class Umum extends Member {
     public Umum(String nama) {
         super(nama, "Umum");
